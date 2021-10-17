6bc2ddc1c0229518de55ce697e2eb82ab7285144
yrathinasamy <60668059+yrathinasamy@users.noreply.github.com>
2021-10-17 15:09:48 +0000

    it works!

---
+++ src/Helloworld.java
@@ -0,0 +1,7 @@
+//package src;
+
+class Helloworld {
+  public static void main (String args[]) {
+    System.out.println("Hello World from Yuvi!!");
+  }
+}
--- src/sample/helloworld.java
+++
@@ -1,7 +0,0 @@
-package src.sample;
-
-class helloworld {
-  public static void main(String args[]) {
-    System.out.println("Hello World from Yuvi!!");
-  }
-}
